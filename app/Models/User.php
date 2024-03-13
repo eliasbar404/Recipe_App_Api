5a2c9a7d61224d6e0d0799d5cc2d2a3b25e86df0
ilyas <ibaraaouche@gmail.com>
2024-03-13 02:34:45 +0000

    setup jwt auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Models\Favourite;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -24,9 +24,14 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'id',
+        'first_name',
+        'last_name',
+        'gender',
+        'is_banned',
         'email',
         'password',
+        'profile_image_url'
     ];
 
     /**
@@ -54,4 +59,34 @@ class User extends Authenticatable
     {
         return $this->hasMany(Favourite::class);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+        return [];
+    }  
 }
