e3e3b53ec36d8ead9bd339a0c4af8cfd87c525f9
ilyas <ibaraaouche@gmail.com>
2024-03-16 00:58:21 +0000

    fix 12

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -121,7 +121,7 @@ class RecipeController extends Controller
         foreach($recipes as $recipe){
             $rating_list = [];
             // if($recipe->Reviews){
-            foreach(Recipe::where('id',$recipe->id)->reviews as $review){
+            foreach($recipe->reviews as $review){
                 array_push($rating_list,$review->rating);
             }
         // }
