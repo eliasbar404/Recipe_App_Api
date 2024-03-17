369b7d7aa7d19aa6b09372517b99c529bf521adf
ilyas <ibaraaouche@gmail.com>
2024-03-17 01:58:49 +0000

    fix 35

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -18,7 +18,7 @@ class RecipeController extends Controller
 {
     //
     public function __construct() {
-        $this->middleware('auth:api');
+        $this->middleware('auth:api',['except' => ['getOneRecipeById', 'getAllRecipes']]);
     }
 
 
